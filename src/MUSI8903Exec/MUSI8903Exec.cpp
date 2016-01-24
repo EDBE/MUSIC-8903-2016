ebd283ade22afe16c72630e0555eb842383e0bce
Liang_Tang <ltangmt@gmail.com>
2016-01-24 10:09:13 -0500

    Implementation for reading and writing audio file

--- src/MUSI8903Exec/MUSI8903Exec.cpp
+++ src/MUSI8903Exec/MUSI8903Exec.cpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[])
 
     long long               iInFileLength       = 0;        //!< length of input file
 
-    clock_t                 time                = 0;
+//    clock_t                 time                = 0;
 
     float                   **ppfAudioData      = 0;
 
@@ -31,25 +31,60 @@ int main(int argc, char* argv[])
 
     //////////////////////////////////////////////////////////////////////////////
     // parse command line arguments
-
+    sInputFilePath = "/Users/musictechnology/Desktop/ShortDataset/Bestial_Lust.wav";
+    sOutputFilePath = "/Users/musictechnology/Desktop/test1.txt";
+    
     //////////////////////////////////////////////////////////////////////////////
     // open the input wave file
-
+    CAudioFileIf::FileIoType_t fileType = CAudioFileIf::kFileRead;
+    CAudioFileIf::create(phAudioFile);
+    
+    phAudioFile -> openFile(sInputFilePath, fileType);
+    
+    CAudioFileIf::FileSpec_t fileSpec;
+    phAudioFile -> getFileSpec(fileSpec);
+    int numChannels = fileSpec.iNumChannels;
+    
+    phAudioFile -> getLength(iInFileLength);
+    
     //////////////////////////////////////////////////////////////////////////////
     // allocate memory
-
+    ppfAudioData = new float* [numChannels];
+    
+    for (int channel = 0; channel < numChannels; channel++) {
+        ppfAudioData[channel] = new float [iInFileLength];
+    }
+    
     // get audio data
+    phAudioFile -> readData(ppfAudioData, iInFileLength);
+    
 
     //////////////////////////////////////////////////////////////////////////////
     // get audio info and print it to stdout
-
+    std::ofstream outfile(sOutputFilePath);
+    for (int channel = 0; channel < numChannels; channel++) {
+        for (int sample = 0; sample < iInFileLength ; sample++) {
+            //            cout << ppfAudioData[channel][sample] << endl;
+            //write the data out
+            //            outfile.write((char*)&ppfAudioData[channel][sample], sizeof(float));
+            outfile <<  ppfAudioData[channel][sample] << endl;
+        }
+    }
+    outfile.close();
+    
     //////////////////////////////////////////////////////////////////////////////
     // do processing
-    cout << "Hello there!" << endl << endl;
+    cout << "Writing is done!" << endl << endl;
 
     //////////////////////////////////////////////////////////////////////////////
     // clean-up
-
+    for (int channel = 0; channel < numChannels; channel++) {
+        delete ppfAudioData[channel];
+    }
+    delete ppfAudioData;
+    
+    CAudioFileIf::destroy(phAudioFile);
+    
     return 0;
     
 }
